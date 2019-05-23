e9f71b6d8a40c4b16b721f4769917ed8068e0998
teresaPap <tp.ch27.m@gmail.com>
2019-05-23 07:06:29 +0300

    comments on nav component

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+	selector: 'app-nav',
+	templateUrl: './nav.component.html',
+	styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+	public username: string;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		// TODO: get user info from auth service and set username variable
+	}
 
 }

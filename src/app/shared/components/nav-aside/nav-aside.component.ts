946d8f2ec2d6427a8ddedf3b18b84086df5744b1
teresaPap <tp.ch27.m@gmail.com>
2019-05-25 13:22:29 +0300

    aside menu basic styling

--- src/app/shared/components/nav-aside/nav-aside.component.ts
+++ src/app/shared/components/nav-aside/nav-aside.component.ts
@@ -7,22 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavAsideComponent implements OnInit {
 
-	public collapseBtnText: 'Expand' | 'Collapse';
-
-	public collapseMenu: boolean;
-
-	public collapseBtnClass: 'fa-angle-double-right' | 'fa-angle-double-left';
+	public expandMenu: boolean;
 
 	constructor() { }
 
 	ngOnInit() {
-		this.collapseBtnText = 'Expand';
-		this.collapseMenu = true;
 	}
 
 	public toggleAsideMenu() {
-		console.log('collapseMenu:' +this.collapseMenu);
-		this.collapseMenu = !this.collapseMenu;
+		this.expandMenu = !this.expandMenu;
 	}
 
 }

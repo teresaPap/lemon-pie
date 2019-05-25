92bb735c706847d2e64f1c743beddbe99a5b681f
teresaPap <tp.ch27.m@gmail.com>
2019-05-25 15:38:26 +0300

    create register component

--- src/app/logon/logon.module.ts
+++ src/app/logon/logon.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from "@angular/core";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    declarations: [ ],
-    imports: [ ],
+    declarations: [ 
+        LoginComponent, 
+        RegisterComponent 
+    ],
+    imports: [ 
+        SharedModule 
+    ],
     providers: [ ]
 })
 export class LogonModule { }

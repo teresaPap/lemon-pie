7dfb8bedaa6854c4fac16566fb549ae455723bfd
teresaPap <tp.ch27.m@gmail.com>
2019-07-01 23:16:29 +0300

    register component

--- src/app/logon/register/register.component.ts
+++ src/app/logon/register/register.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/shared/custom-validators';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+	public registerForm: FormGroup; 
 
-  ngOnInit() {
-  }
+	constructor( 
+		private fb : FormBuilder,
+		private authService: AuthService ) { }
+
+	ngOnInit() {
+
+		this.registerForm = this.fb.group({
+			email: ['', Validators.required],
+			password: ['', Validators.required],
+			confirmPassword: ['']
+		},  { validator: CustomValidators.comparePasswords });
+
+	}
+
+	public register() {
+		this.authService.register(this.registerForm.value).then(
+			res => {
+				console.log('Register success!', res);
+			},
+			err => console.log('An error has occured', err)
+		);
+	}
 
 }
---
+++ src/app/shared/custom-validators.ts
@@ -0,0 +1,16 @@
+import { AbstractControl } from "@angular/forms";
+
+export class CustomValidators {
+
+    static comparePasswords( control: AbstractControl ) :  {[key:string]: any} | null {
+        const password = control.get('password');
+        const confirmPassword = control.get('confirmPassword');
+
+        if ( password.pristine || confirmPassword.pristine ) return null;
+        
+        if ( password.value == confirmPassword.value) return null;
+        
+        return { 'comparePasswords': true }
+
+    } 
+}

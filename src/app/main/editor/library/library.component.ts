c2a160f3636ac49d31cc9a536472cf35ccc99ef0
teresaPap <teresapap27@gmail.com>
2019-07-20 11:19:53 +0300

    clean up library component

--- src/app/main/editor/library/library.component.ts
+++ src/app/main/editor/library/library.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FileUploadService } from '../../../shared/services/file-upload.service';
-import { DomSanitizer } from '@angular/platform-browser';
+
 
 @Component({
 	selector: 'app-library',
@@ -9,62 +8,13 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class LibraryComponent implements OnInit {
 
-	public fileToUpload: File = null;
-	public imgPreview;
+	public projects = [];
 
-	constructor( 
-		private fileUploader: FileUploadService,
-		private domSanitizer: DomSanitizer ) { }
+	constructor() { }
 
 	ngOnInit() {
+		this.projects = [];
 	}
-
-	handleFileInput(files: FileList) {
-        if (files.length > 0) {
-            let file: File = files[0];
-            if (this.validImage(file)) {
-
-				console.log(file);
-				let fileUrl = window.URL.createObjectURL(file);
-				this.imgPreview = this.domSanitizer.bypassSecurityTrustResourceUrl(fileUrl);
-				
-            } else {
-				console.warn('not a valid image');
-				// this.uploadFile.nativeElement.value = '';
-            }
-        }
-	}
-
-
-	uploadFile() {
-		this.fileUploader.postFile(this.fileToUpload).subscribe(
-			d => {
-				// do something, if upload success
-			}, 
-			e => console.error('Upload File Error: ' , e ));
-	}
-
-	// readURL(files: FileList) {
-    //     if (files && files[0]) {
-    //         var reader = new FileReader();
-
-	// 		reader.onload
-			
-	// 		{  e => 
-    //             document.getElementById('blah').src = e.target.result
-    //         };
-
-    //         reader.readAsDataURL(input.files[0]);
-    //     }
-    // }
-
-	private validImage(file: File): boolean {
-		// TODO: add other validations (eg file size) if needed
-
-		console.log( '57', file.type.indexOf('image') );
-        if ( file.type.indexOf('image') > -1)  return true;
-        return false;
-    }
 
 
 }

7006b3662d547f58d178283015b9dcd1bddd2603
teresaPap <teresapap27@gmail.com>
2019-08-17 11:07:16 +0300

    file edit functionality

--- src/app/main/editor/project-detail/project-detail.component.ts
+++ src/app/main/editor/project-detail/project-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FilesService } from '../../../shared/data-services/files.service';
 import { IProject } from '../../../shared/interfaces/IProject';
 import { ProjectsService } from '../../../shared/data-services/projects.service';
 import { switchMap, tap } from 'rxjs/operators';
 import { forkJoin, Observable } from 'rxjs';
 import { IFile } from '../../../shared/interfaces/IFile';
+import { StorageService } from '../../../shared/services/storage.service';
 
 // PAGE DESCRIPTION: 
 // In this view the user can add files to the current project
@@ -21,9 +22,11 @@ export class ProjectDetailComponent implements OnInit {
 	public files: IFile[] = [];
 
 	constructor(
+		public storage: StorageService,
+		public router: Router, 
 		private route: ActivatedRoute,
 		private fileCtrl: FilesService,
-		private projectCtrl: ProjectsService
+		private projectCtrl: ProjectsService,
 	) { }
 
 	ngOnInit() {
@@ -40,10 +43,17 @@ export class ProjectDetailComponent implements OnInit {
 				this.project = { ...this.project , ...res[0].data() };
 				console.log("Editing project: " , this.project );
 				if (res[1]) this.files = res[1];
+				console.log(this.files);
 			})
 		).subscribe();
 	}
 
+	public navToEdit(file: IFile) {
+		this.storage.store( file.id , file);
+		// this.router.navigate( ['/edit1'], {relativeTo: this.route} ); //, { queryParams: [ {fileId: file.id} ] ,  );
+	}
+
+	
 	private getFiles(projectId: string) {
 		return this.fileCtrl.read(projectId);
 	}
--- src/app/main/editor/project-edit/project-edit.component.ts
+++ src/app/main/editor/project-edit/project-edit.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { StorageService } from '../../../shared/services/storage.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { IFile } from '../../../shared/interfaces/IFile';
 
 
 // PAGE DESCRIPTION: 
-// In this view the user can select a file and open the file editor view (to be developed). 
+// In this view the user can select a file and open the file editor view. 
 
 
 @Component({
@@ -12,16 +14,40 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProjectEditComponent implements OnInit {
 
+	// @ViewChild('editor') editor: ElementRef ; 
+
+	public file: IFile;
+
 	constructor( 
-		private route: ActivatedRoute,
+		public storage: StorageService,
+		public router: Router,
+		private route: ActivatedRoute
 		) { }
 
 	ngOnInit() {
-		// this.route.params.subscribe( params => {
-		// 	console.log("Edidting project with id: " + params.id);
-		// })
+		this.route.queryParamMap.subscribe(res => {
+			this.file = this.storage.load( res.get('id') )
+			console.log(res.get('id'), this.file );
+			this.setBackground(this.file.downloadURL)
+		})
 	}
 
-	
+	// #region - Canvas Manipulation Functions
+
+	private setBackground(url:string) {
+		
+		// const ctx = this.editor.getContext('2d');
+		// ctx.setBackground(url);
+	}
+
+
+
+	// #endregion
+
+
+	// var canvas = document.getElementById("canvas");
+	// var ctx = canvas.getContext("2d");
+	// ctx.fillStyle = "blue";
+	// ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 }

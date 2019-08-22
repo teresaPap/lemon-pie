b88b791851dcecf67e7b51d813449658508bb010
teresaPap <teresapap27@gmail.com>
2019-08-22 10:02:28 +0300

    set up select menu component

--- src/app/shared/components/selection-menu/selection-menu.component.ts
+++ src/app/shared/components/selection-menu/selection-menu.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { IFile } from '../../interfaces/IFile';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
 	selector: 'app-selection-menu',
@@ -7,15 +9,21 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class SelectionMenuComponent implements OnInit {
 
+	public files: IFile[];
 	public linkToFileForm: FormGroup;
 
-	constructor( private fb: FormBuilder ) { 
+	constructor( 
+		private fb: FormBuilder, 
+		public storage: StorageService ) {
+
 		this.linkToFileForm = this.fb.group({
 			fileName: ''
 		})
 	}
 
 	ngOnInit() {
+		const projectId:string = this.storage.load('activeProjectId');
+		this.files = this.storage.load(projectId);
 	}
 
 	public submitLinkToFileForm() {

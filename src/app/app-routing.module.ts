79f502de00c2ec38e819c64095f061bac09c6eef
teresaPap <teresapap27@gmail.com>
2020-02-09 11:24:56 +0200

    update font styling, create register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
 const routes: Routes = [
-	{ path: 'login', component : LoginComponent }
+	{ path: 'login', component : LoginComponent },
+	{ path: 'register', component : RegisterComponent },
 ];
 
 

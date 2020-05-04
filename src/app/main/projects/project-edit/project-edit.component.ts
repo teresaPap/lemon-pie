96efc7a0684eea3cb43d8f413fd44c6788d3cf73
teresaPap <teresapap27@gmail.com>
2020-05-04 14:26:01 +0300

    refactor project edit component event naming

--- src/app/main/projects/project-edit/project-edit.component.ts
+++ src/app/main/projects/project-edit/project-edit.component.ts
@@ -2,12 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NotifierService } from 'angular-notifier';
 import { StorageService } from '../../../shared/services/storage.service';
-import { IFile } from '../../../shared/interfaces/IFile';
 import { FilesService } from '../../../shared/data-services/files.service';
-
-
-// PAGE DESCRIPTION:
-// In this view the user can see the file editor view and edit panel (buttons etc).
+import { IFile } from '../../../shared/interfaces/IFile';
 
 
 @Component({
@@ -32,21 +28,28 @@ export class ProjectEditComponent implements OnInit {
 		this.setActiveFile(this.storage.load('activeFile'));
 	}
 
-	public saveArea(event): void {
-		this.filesCtrl.saveFileLink(event).subscribe(
-			() => {
-				console.log('TODO: show user friendly success message');
-				this.notifier.notify('success', 'Link was saved successfully!');
-			},
-			err => {
-				console.log('TODO: show user friendly failure message');
-				this.notifier.notify('error', `${err.message}`);
-			}
-		);
+	public saveSingleChangeLocally(event): void {
+
+		console.log(event);
+
+		// this.filesCtrl.saveFileLink(event).subscribe(
+		// 	() => {
+		// 		this.notifier.notify('success', 'Link was saved successfully!');
+		// 	},
+		// 	err => {
+		// 		console.log('TODO: show user friendly failure message');
+		// 		this.notifier.notify('error', `${err.message}`);
+		// 	}
+		// );
 	}
+
 
 	public onShowLinks(event): void {
 		this.showLinks = event;
+	}
+
+	public onSaveAllChanges(): void {
+		// TODO: use firebase data services to update links, files and project according to the data stored in storage service.
 	}
 
 	public onChangeActiveFile(file): void {
@@ -54,7 +57,10 @@ export class ProjectEditComponent implements OnInit {
 	}
 
 	private setActiveFile(file: IFile) {
+		console.log('hey');
 		this.file = file;
+
+
 		this.filesCtrl.getFileLinks(this.file.id).subscribe(
 			res => this.links = res
 		);

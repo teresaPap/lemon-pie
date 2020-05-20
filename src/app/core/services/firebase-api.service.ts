25e88544fb800a882e51900e908b055f7f3de8d9
teresaPap <teresapap27@gmail.com>
2020-05-20 13:34:27 +0300

    refactor firebase api create document and a reference to a parent document

--- src/app/core/services/firebase-api.service.ts
+++ src/app/core/services/firebase-api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from "@angular/fire/firestore";
-import {Observable, forkJoin, from, of, iif} from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable, from, iif } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
 
-import * as firebase from "firebase";
+
+import * as firebase from 'firebase';
 
 
 @Injectable()
@@ -17,24 +18,24 @@ export class FirebaseApiService {
 		public firestore: AngularFirestore,
 	) { }
 
-	public createDocument(fields: any, collectionName: string, parentDocumentName?: string): Observable<any> {
+	public createDocument(documentFields: any, collectionPath: string, parentDocPath?: string): Observable<any> {
+    	const collectionRef: AngularFirestoreCollection = this.firestore.collection(collectionPath);
+		const parentDocumentRef: AngularFirestoreDocument = this.firestore.doc(parentDocPath);
 
-    	const collectionRef: AngularFirestoreCollection = this.firestore.collection(collectionName);
-		const parentDocumentRef: AngularFirestoreDocument = this.firestore.doc(parentDocumentName);
-
-		return from( collectionRef.add(fields) ).pipe(
-			tap((documentRef: firebase.firestore.DocumentReference) => {
-				if (parentDocumentName) {
-					parentDocumentRef.update({
+		return from( collectionRef.add(documentFields) ).pipe(
+			tap((documentRef: firebase.firestore.DocumentReference) =>
+				iif(() => !!parentDocPath,
+					from(parentDocumentRef.update({
 						'references': firebase.firestore.FieldValue.arrayUnion(documentRef)
-					})
-				}
-			}),
+					}))
+				),
+			),
+			tap( res => console.log(res)),
 			switchMap((documentRef: firebase.firestore.DocumentReference) => documentRef.get() ),
 			map((documentData: firebase.firestore.DocumentData) => documentData.data() ),
 			catchError( err => err )
 		);
+    }
 
-	}
 
 }

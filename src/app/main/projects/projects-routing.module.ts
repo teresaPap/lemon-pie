b4e1768c31a852e5c2d0755ce975b20e32d11c15
teresaPap <teresapap27@gmail.com>
2020-05-23 12:33:17 +0300

    create project details page from scratch

--- src/app/main/projects/projects-routing.module.ts
+++ src/app/main/projects/projects-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectCreateComponent } from './project-create/project-create.component';
-import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 import { ProjectEditComponent } from './project-edit/project-edit.component';
 import { ProjectPresentComponent } from './project-present/project-present.component';
 
@@ -11,9 +11,8 @@ const routes: Routes = [
 	{ path: 'new', component : ProjectCreateComponent },
 	{ path: ':id/present', component : ProjectPresentComponent },
 	{ path: ':id/edit', component : ProjectEditComponent },
-	{ path: ':id', component : ProjectDetailComponent },
+	{ path: ':id', component : ProjectDetailsComponent },
 	{ path: '', component : ProjectListComponent },
-
 ];
 
 @NgModule({
--- src/app/main/projects/projects.module.ts
+++ src/app/main/projects/projects.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { ProjectListComponent } from './project-list/project-list.component';
-import { ProjectDetailComponent } from './project-detail/project-detail.component';
 import { ProjectEditComponent } from './project-edit/project-edit.component';
-import { FileListItemComponent } from './project-detail/file-list-item/file-list-item.component';
+import { FileListItemComponent } from './project-detail-DEPRECATED/file-list-item/file-list-item.component';
 import { ProjectPresentComponent } from './project-present/project-present.component';
 import { ProjectCreateComponent } from './project-create/project-create.component';
 import { SharedModule } from '../../shared/shared.module';
 import { ProjectsRoutingModule } from './projects-routing.module';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 
 
 @NgModule({
 	declarations: [
 		ProjectListComponent,
-		ProjectDetailComponent,
 	  	ProjectEditComponent,
 	  	FileListItemComponent,
 		ProjectPresentComponent,
 		ProjectCreateComponent,
+		ProjectDetailsComponent,
   	],
   	imports: [
   		SharedModule,

64491a7903f740b7ebbf215c4b41c21a7ec91a33
teresaPap <teresapap27@gmail.com>
2020-05-26 15:40:21 +0300

    routing project details component - wip

--- src/app/main/projects/projects-routing.module.ts
+++ src/app/main/projects/projects-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectCreateComponent } from './project-create/project-create.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
-import { ProjectEditComponent } from './project-edit/project-edit.component';
-import { ProjectPresentComponent } from './project-present/project-present.component';
 
 
 const routes: Routes = [
-	{ path: 'new', component : ProjectCreateComponent },
-	{ path: ':id/present', component : ProjectPresentComponent },
-	{ path: ':id/edit', component : ProjectEditComponent },
+	{ path: 'new', component: ProjectCreateComponent },
+	{ path: ':id/flow-preview' },
+	{ path: ':id/flow-edit' },
+	{ path: ':id/files-inspect' },
+	{ path: ':id/files-add' },
+	{ path: ':id/edit' },
+	{ path: ':id/delete' },
 	{ path: ':id', component : ProjectDetailsComponent },
 	{ path: '', component : ProjectListComponent },
 ];

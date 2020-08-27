e28bddff1654d70b625acf5038db71367fdda671
teresaPap <teresapap27@gmail.com>
2020-08-27 11:25:29 +0300

    setup firebase functions

---
+++ functions/README.md
@@ -0,0 +1,14 @@
+# Firebase Functions
+
+At this point, firebase functions are free only for node v8.
+Lemon Pie project does not utilise firebase functions at this point. 
+
+## Prerequisites
+- NodeJs v8
+- ...
+
+## Usage
+`cd ~/lemon-pie/functions && npm start`
+
+
+Read more in firebase [docs](https://firebase.google.com/docs/functions).
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,37 @@
 import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+
+
+admin.initializeApp(functions.config().firebase);
+
+const firestore = admin.firestore();
+
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+	response.send("Hello from Firebase!!");
+});
+
+
+export const getAllUsers = functions.https.onRequest(async (request, response) => {
+	const snapshot = await firestore.collection('users').get();
+	const users = snapshot.docs.map(doc => ( {id: doc.id, ...doc.data() }) );
+
+	response.json({ users });
+});
+
+// export const getProjects = functions.https.onRequest(async (request, response) => {
+// 	const snapshot = await firestore.collection()
+// })
+
+export const deleteUser = functions.firestore.document('users/{userId}').onDelete( async (snapshot, context) => {
+	const subCollection = snapshot.get('projects');
+	console.log('subCollection', subCollection);
+
+	return 'My function runs smoothly';
+});
+
+
+
